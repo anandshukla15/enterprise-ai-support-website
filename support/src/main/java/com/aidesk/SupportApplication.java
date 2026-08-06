133687db1a1c655996ae434a41f9dc3b2cbd078d
anandshukla15 <anandshukla1742@gmail.com>
2026-08-06 23:23:59 +0530

    updated databse

--- support/src/main/java/com/aidesk/SupportApplication.java
+++ support/src/main/java/com/aidesk/SupportApplication.java
@@ -1,4 +1,4 @@
-package com.anand.support;
+package com.aidesk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- support/src/test/java/com/aidesk/SupportApplicationTests.java
+++ support/src/test/java/com/aidesk/SupportApplicationTests.java
@@ -1,4 +1,4 @@
-package com.anand.support;
+package com.aidesk;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

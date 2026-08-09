66784cbd280745fcbfde6137608e4dfa74284901
anandshukla15 <anandshukla1742@gmail.com>
2026-08-10 01:21:53 +0530

    company response done

--- support/src/main/java/com/aidesk/company/dto/CompanyResponse.java
+++ support/src/main/java/com/aidesk/company/dto/CompanyResponse.java
@@ -1,4 +1,13 @@
 package com.aidesk.company.dto;
 
-public class CompanyResponse {
+import java.time.LocalDateTime;
+
+public record CompanyResponse (
+        Long id,
+        String name,
+        String email,
+        String status,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
+){
 }

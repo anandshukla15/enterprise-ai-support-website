2cea9a49976ba8a916c9e9da19551e5782e732b5
anandshukla15 <anandshukla1742@gmail.com>
2026-08-09 22:28:44 +0530

    writing exception handling

--- support/src/main/java/com/aidesk/exception/custom/ResourceNotFoundException.java
+++ support/src/main/java/com/aidesk/exception/custom/ResourceNotFoundException.java
@@ -1,4 +1,7 @@
 package com.aidesk.exception.custom;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
 }

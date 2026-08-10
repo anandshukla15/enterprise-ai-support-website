3e6a6ef4d89455f5a4d7af0d420d6e06301421a7
anandshukla15 <anandshukla1742@gmail.com>
2026-08-10 23:08:10 +0530

    controller  updated

--- support/src/main/java/com/aidesk/company/controller/CompanyController.java
+++ support/src/main/java/com/aidesk/company/controller/CompanyController.java
@@ -1,4 +1,53 @@
 package com.aidesk.company.controller;
 
+
+import com.aidesk.common.dto.ApiResponse;
+import com.aidesk.company.dto.CompanyResponse;
+import com.aidesk.company.dto.CreateCompanyRequest;
+import com.aidesk.company.service.CompanyService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/companies")
+@RequiredArgsConstructor
 public class CompanyController {
+
+    private final CompanyService companyService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<CompanyResponse>> createCompany(
+            @Valid @RequestBody CreateCompanyRequest request) {
+
+        CompanyResponse response =
+                companyService.createCompany(request);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(ApiResponse.<CompanyResponse>builder()
+                        .success(true)
+                        .message("Company created successfully")
+                        .data(response)
+                        .build());
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse<CompanyResponse>> getCompany(
+            @PathVariable Long id) {
+
+        CompanyResponse response =
+                companyService.getCompanyById(id);
+
+        return ResponseEntity.ok(
+                ApiResponse.<CompanyResponse>builder()
+                        .success(true)
+                        .message("Company fetched successfully")
+                        .data(response)
+                        .build()
+        );
+    }
 }

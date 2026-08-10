63a6ed7be7ab7a9e1114bc8f69238fa5b05542ee
anandshukla15 <anandshukla1742@gmail.com>
2026-08-10 21:29:21 +0530

    company service file

--- support/src/main/java/com/aidesk/company/service/CompanyService.java
+++ support/src/main/java/com/aidesk/company/service/CompanyService.java
@@ -1,4 +1,11 @@
 package com.aidesk.company.service;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class CompanyService {
+
 }

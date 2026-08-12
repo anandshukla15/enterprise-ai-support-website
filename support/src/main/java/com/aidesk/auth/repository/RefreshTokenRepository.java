79922d452ed801ed1f700a4ff82fdfbee8d5fc63
anandshukla15 <anandshukla1742@gmail.com>
2026-08-13 00:03:41 +0530

    refresh token repository

--- support/src/main/java/com/aidesk/auth/repository/RefreshTokenRepository.java
+++ support/src/main/java/com/aidesk/auth/repository/RefreshTokenRepository.java
@@ -1,4 +1,14 @@
 package com.aidesk.auth.repository;
 
-public class RefreshTokenRepository {
+import com.aidesk.auth.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository
+extends JpaRepository<RefreshToken,Long> {
+
+    Optional<RefreshToken> findByToken(String token);
+
+    void deleteAllByUserId(Long userId);
 }

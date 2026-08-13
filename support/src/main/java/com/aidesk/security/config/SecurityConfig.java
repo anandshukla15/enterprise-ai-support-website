b30c7124671c0da4ff0647b25f9fbdeff7a701d0
anandshukla15 <anandshukla1742@gmail.com>
2026-08-14 00:57:53 +0530

    security added

--- support/src/main/java/com/aidesk/security/config/SecurityConfig.java
+++ support/src/main/java/com/aidesk/security/config/SecurityConfig.java
@@ -1,25 +1,45 @@
 package com.aidesk.security.config;
 
+import com.aidesk.security.filter.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(
+                                SessionCreationPolicy.STATELESS
+                        )
+                )
+
 
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/health",
-                                "/api/companies/**"
+                                "/api/companies/**","/api/auth/register",
+                                "/api/auth/login",
+                                "/api/auth/refresh"
                         ).permitAll()
                         .anyRequest().authenticated()
+                )
+
+                .addFilterBefore(
+                        jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class
                 );
 
         return http.build();

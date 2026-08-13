5b4abc296cb605700c387270a07a9cc86a35883b
anandshukla15 <anandshukla1742@gmail.com>
2026-08-13 23:07:14 +0530

    customuserdetailsservice file

---
+++ support/src/main/java/com/aidesk/security/service/CustomUserDetailsService.java
@@ -0,0 +1,42 @@
+package com.aidesk.security.service;
+
+import com.aidesk.user.entity.User;
+import com.aidesk.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService
+        implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(
+                                "User not found"
+                        )
+                );
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .authorities(
+                        new SimpleGrantedAuthority(
+                                "ROLE_" + user.getRole().name()
+                        )
+                )
+                .disabled(!user.isEnabled())
+                .build();
+    }
+}

3937b9e2b81d71146dddf646797d5df3e71e24a0
anandshukla15 <anandshukla1742@gmail.com>
2026-08-15 01:56:27 +0530

    RestAuthenticationEntryPoint done $ maven updated

--- support/src/main/java/com/aidesk/security/handler/RestAuthenticationEntryPoint.java
+++ support/src/main/java/com/aidesk/security/handler/RestAuthenticationEntryPoint.java
@@ -1,4 +1,53 @@
 package com.aidesk.security.handler;
 
-public class RestAuthenticationEntryPoint {
+
+import com.aidesk.common.dto.ApiResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
+    public RestAuthenticationEntryPoint(
+            ObjectMapper objectMapper) {
+
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException)
+            throws IOException {
+
+        response.setStatus(
+                HttpServletResponse.SC_UNAUTHORIZED
+        );
+
+        response.setContentType(
+                MediaType.APPLICATION_JSON_VALUE
+        );
+
+        ApiResponse<Void> body =
+                ApiResponse.<Void>builder()
+                        .success(false)
+                        .message("Authentication required")
+                        .data(null)
+                        .build();
+
+        response.getWriter().write(
+                objectMapper.writeValueAsString(body)
+        );
+    }
+
 }

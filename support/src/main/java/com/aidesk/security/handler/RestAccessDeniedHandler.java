2483c9f65d19d7d1902373149ad6b253e461a86d
anandshukla15 <anandshukla1742@gmail.com>
2026-08-15 01:59:15 +0530

    RestAccessDeniedHandler done

---
+++ support/src/main/java/com/aidesk/security/handler/RestAccessDeniedHandler.java
@@ -0,0 +1,18 @@
+package com.aidesk.security.handler;
+
+
+
+import com.aidesk.common.dto.ApiResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class RestAccessDeniedHandler {
+}

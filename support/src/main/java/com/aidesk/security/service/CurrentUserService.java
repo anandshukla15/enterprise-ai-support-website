3e8d7ea89b0fd93477c9208f8efd79cf8b9f1d9a
anandshukla15 <anandshukla1742@gmail.com>
2026-08-15 01:14:19 +0530

    cureent userservice

--- support/src/main/java/com/aidesk/security/service/CurrentUserService.java
+++ support/src/main/java/com/aidesk/security/service/CurrentUserService.java
@@ -1,4 +1,36 @@
 package com.aidesk.security.service;
 
+
+import com.aidesk.exception.custom.ResourceNotFoundException;
+import com.aidesk.user.entity.User;
+import com.aidesk.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class CurrentUserService {
+
+    private final UserRepository userRepository;
+
+    public User getCurrentUser(Authentication authentication) {
+
+        if (authentication == null ||
+                !authentication.isAuthenticated()) {
+
+            throw new ResourceNotFoundException(
+                    "Authenticated user not found"
+            );
+        }
+
+        String email = authentication.getName();
+
+        return userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException(
+                                "Authenticated user not found"
+                        )
+                );
+    }
 }

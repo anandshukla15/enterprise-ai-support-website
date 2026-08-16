53afa3f1f04a50dbdf28a1acf8fbd159d6de7155
anandshukla15 <anandshukla1742@gmail.com>
2026-08-16 09:28:35 +0530

    TenantSecurityService.java , for access of company

---
+++ support/src/main/java/com/aidesk/security/service/TenantSecurityService.java
@@ -0,0 +1,29 @@
+package com.aidesk.security.service;
+
+import com.aidesk.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TenantSecurityService {
+
+    private final CurrentUserService currentUserService;
+
+    public boolean hasAccessToCompany(
+            Long companyId,
+            Authentication authentication) {
+
+        User user =
+                currentUserService.getCurrentUser(authentication);
+
+        if (user.getCompany() == null) {
+            return false;
+        }
+
+        return user.getCompany()
+                .getId()
+                .equals(companyId);
+    }
+}

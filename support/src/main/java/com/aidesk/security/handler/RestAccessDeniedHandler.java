12ef85e0156142514cabdd1c5b5b08042ba69782
anandshukla15 <anandshukla1742@gmail.com>
2026-08-20 00:03:51 +0530

    solvd and running

--- support/src/main/java/com/aidesk/security/handler/RestAccessDeniedHandler.java
+++ support/src/main/java/com/aidesk/security/handler/RestAccessDeniedHandler.java
@@ -3,7 +3,7 @@ package com.aidesk.security.handler;
 
 
 import com.aidesk.common.dto.ApiResponse;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.MediaType;
--- support/src/main/java/com/aidesk/security/handler/RestAuthenticationEntryPoint.java
+++ support/src/main/java/com/aidesk/security/handler/RestAuthenticationEntryPoint.java
@@ -2,8 +2,7 @@ package com.aidesk.security.handler;
 
 
 import com.aidesk.common.dto.ApiResponse;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.fasterxml.jackson.databind.ObjectMapper;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.MediaType;

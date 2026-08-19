0a45ee671555595ccc9fe90a5642f6cae0133cf4
anandshukla15 <anandshukla1742@gmail.com>
2026-08-20 00:34:51 +0530

    customer entity

---
+++ support/src/main/java/com/aidesk/customer/entity/Customer.java
@@ -0,0 +1,40 @@
+package com.aidesk.customer.entity;
+
+import com.aidesk.common.entity.BaseEntity;
+import com.aidesk.company.entity.Company;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Table(
+        name = "customers",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_customer_company_email",
+                        columnNames = {"company_id", "email"}
+                )
+        }
+)
+@Getter
+@Setter
+public class Customer extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "company_id", nullable = false)
+    private Company company;
+
+    @Column(nullable = false, length = 150)
+    private String name;
+
+    @Column(nullable = false, length = 255)
+    private String email;
+
+    @Column(length = 30)
+    private String phone;
+}

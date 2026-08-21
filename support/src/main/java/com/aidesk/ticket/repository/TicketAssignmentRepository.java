2755a0daa18e3691cfc92c629fef8d68a53b9415
anandshukla15 <anandshukla1742@gmail.com>
2026-08-21 23:31:38 +0530

    repository done

--- support/src/main/java/com/aidesk/ticket/repository/TicketAssignmentRepository.java
+++ support/src/main/java/com/aidesk/ticket/repository/TicketAssignmentRepository.java
@@ -3,6 +3,10 @@ package com.aidesk.ticket.repository;
 import com.aidesk.ticket.entity.TicketAssignment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TicketAssignmentRepository
         extends JpaRepository<TicketAssignment, Long> {
+
+    List<TicketAssignment> findByTicketIdOrderByCreatedAtDesc(Long ticketId);
 }

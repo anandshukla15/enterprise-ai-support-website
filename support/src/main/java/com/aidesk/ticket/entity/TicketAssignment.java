191fe4b3cd60477199d6736ab94ec8ce061b5f30
anandshukla15 <anandshukla1742@gmail.com>
2026-08-21 23:23:10 +0530

    ticket assignment entity done

--- support/src/main/java/com/aidesk/ticket/entity/TicketAssignment.java
+++ support/src/main/java/com/aidesk/ticket/entity/TicketAssignment.java
@@ -1,4 +1,31 @@
 package com.aidesk.ticket.entity;
 
-public class TicketAssignment {
+
+import com.aidesk.common.entity.BaseEntity;
+import com.aidesk.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "ticket_assignments")
+@Getter
+@Setter
+public class TicketAssignment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "ticket_id", nullable = false)
+    private Ticket ticket;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "assigned_agent_id", nullable = false)
+    private User assignedAgent;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "assigned_by_user_id", nullable = false)
+    private User assignedBy;
 }
